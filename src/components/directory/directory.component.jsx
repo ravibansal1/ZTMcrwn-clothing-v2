a4d321380e3f2361fa42f756c627f73e09e5158f
Ravi Bansal <ravibansal+1@gmail.com>
2023-06-29 15:03:06 -0500

    added directory/category item component & background images

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,12 @@
+import './directory.styles.scss'
+import CategoryItem from '../category-item/category-item.component'
+
+export default function Directory({ categories }) {
+    return (
+        <div className='categories-container'>
+            {categories.map((category) => {
+                return <CategoryItem key={category.id} category={category} />
+            })}
+        </div>
+    )
+}
